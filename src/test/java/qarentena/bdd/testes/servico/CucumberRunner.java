9554e4f719844fa135d30bd659a26e610e1a8ac2
Rafael Lima <rlima@transferwise.com>
2020-07-11 15:26:58 +0100

    Adiciona estrutura basica do cucumber

---
+++ src/test/java/qarentena/bdd/testes/servico/CucumberRunner.java
@@ -0,0 +1,10 @@
+package qarentena.bdd.testes.servico;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+public class CucumberRunner {
+}
