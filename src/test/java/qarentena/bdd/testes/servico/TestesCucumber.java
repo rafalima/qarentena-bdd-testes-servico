44c906b1fa92e6217602095e05d688c9001bc5d5
Rafael Lima <rlima@transferwise.com>
2020-07-11 21:51:07 +0100

    Adiciona Cucumber Runner

--- src/test/java/qarentena/bdd/testes/servico/TestesCucumber.java
+++ src/test/java/qarentena/bdd/testes/servico/TestesCucumber.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"pretty", "html:build/reports/feature"},
+@CucumberOptions(tags = "not @wip and not @quarentena",
+        plugin = {"pretty", "html:build/reports/feature.html"},
         features = {"src/test/java/qarentena/bdd/testes/servico/features"})
-public class CucumberRunner {
+public class TestesCucumber {
 }
