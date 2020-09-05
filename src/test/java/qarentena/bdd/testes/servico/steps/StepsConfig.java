e9b3b739f04fd0b81df3dec2a4e155ccf58449e7
Rafael Lima <rafalima07@gmail.com>
2020-09-05 14:53:05 +0100

    Add owner library

--- src/test/java/qarentena/bdd/testes/servico/steps/StepsConfig.java
+++ src/test/java/qarentena/bdd/testes/servico/steps/StepsConfig.java
@@ -6,10 +6,9 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
-import org.aeonbits.owner.ConfigFactory;
-import qarentena.bdd.testes.servico.suporte.config.ServerConfig;
 import qarentena.bdd.testes.servico.suporte.api.PetApi;
 import qarentena.bdd.testes.servico.suporte.config.ConfigManager;
+import qarentena.bdd.testes.servico.suporte.config.ServerConfig;
 
 import static io.restassured.RestAssured.basePath;
 import static io.restassured.RestAssured.baseURI;
