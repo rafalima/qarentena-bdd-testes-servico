d3ebbf8197b7a780428e9a9ea2883ac9b3591c0b
Rafael Lima <rafalima07@gmail.com>
2020-09-12 21:01:52 +0100

    Modify tasks

--- src/test/java/qarentena/bdd/testes/servico/TestesCucumber.java
+++ src/test/java/qarentena/bdd/testes/servico/TestesCucumber.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         tags = "not @wip and not @quarentena",
-        plugin = {"pretty", "html:reports/feature.html"},
+        plugin = {"pretty", "html:build/reports/feature.html"},
         features = {"src/test/resources/features"})
 public class TestesCucumber {
 }
